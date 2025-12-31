aa50afceb497464cfe50979904150625942f74bd
vekin-devops <vekin-devops@vekin-devopss-MacBook-Pro.local>
2025-12-31 22:03:14 +0700

    chore: create more example sel-signed cert for more dns host name

--- create_self_signed_cert/start_create.sh
+++ create_self_signed_cert/start_create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME="psql_tlsssl" \
-DNS_NAMES="postgres,localhost,db.task-mng.internal" \
+DNS_NAMES="postgres,localhost,db.task-mng.internal,host.docker.internal" \
 IP_ADDRESSES="127.0.0.1,10.0.0.10" \
 ./create_self_sign_cert.sh
